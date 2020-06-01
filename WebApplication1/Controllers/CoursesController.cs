de142acdf94b803cd53fb6e2522916cb62b47936
BigSchool <vnti262@gmail.com>
2020-06-01 11:29:22 +0700

    bac

---
+++ WebApplication1/Controllers/CoursesController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+using WebApplication1.ViewModel;
+
+namespace WebApplication1.Controllers
+{
+    public class CoursesController : Controller
+    {
+        private readonly     ApplicationDbContext _dbContext;
+        public CoursesController()
+        
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        // GET: Courses
+        public ActionResult Create()
+        
+        {
+            var viewModel = new CourseViewModels
+            {
+                Categories = _dbContext.Categories.ToList()
+            };
+            return View(viewModel);
+        }
+    }
+}
---
+++ WebApplication1/ViewModel/CourseViewModels.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication1.Models;
+
+namespace WebApplication1.ViewModel
+{
+    public class CourseViewModels
+    {
+        public string place { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public byte Category { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
+        public DateTime GetDateTime()
+        {
+            return DateTime.Parse(string.Format("{0} {1}", Date, Time));
+        }
+    }
+}
